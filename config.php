598dbda7e25e4169e40dcc5db0e2b8d39a2b130d
aasit <aasit@localhost-aasitin>
2018-09-14 16:49:52 +0530

    code commit

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.1.159/OMS/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.1.159/OMS/');
+
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/OMS/catalog/');
+define('DIR_SYSTEM', '/var/www/html/OMS/system/');
+define('DIR_LANGUAGE', '/var/www/html/OMS/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/OMS/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/OMS/system/config/');
+define('DIR_IMAGE', '/var/www/html/OMS/image/');
+define('DIR_CACHE', '/var/www/html/OMS/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/OMS/system/download/');
+define('DIR_DOWNLOAD_PDF', '/var/www/html/OMS/system/download/');
+define('DIR_UPLOAD', '/var/www/html/OMS/system/upload/');
+define('DIR_LOGS', '/var/www/html/OMS/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/OMS/system/modification/');
+
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Mysql#321');
+define('DB_DATABASE', 'OMS_LOCAL');
+//define('DB_DATABASE', 'kai_dummy');
+define('DB_PREFIX', 'oc_');
